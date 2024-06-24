3d7ff137357763d901b1ad797473cfbe69db4eeb
Yure <yurecod@gmail.com>
2024-06-24 17:10:03 -0400

    start project

---
+++ src/App.tsx
@@ -0,0 +1,108 @@
+import { PlusCircle } from '@phosphor-icons/react'
+import { useState } from 'react'
+
+import styles from './App.module.css'
+
+import { Empty } from './components/List/Empty/Empty'
+import { Header as ListHeader } from './components/List/Header/Header'
+import { Button } from './components/Main/Button/Button'
+import { Header } from './components/Main/Header/Header'
+import { Input } from './components/Main/Input/Input'
+import { Item } from './components/List/Item/Item'
+
+export interface ITask {
+  id: number
+  text: string
+  isChecked: boolean
+}
+
+export function App() {
+  const [tasks, setTasks] = useState<ITask[]>([])
+  const [inputValue, setInputValue] = useState('')
+
+  const checkedTasksCounter = tasks.reduce((prevValue, currentTask) => {
+    if (currentTask.isChecked) {
+      return prevValue + 1
+    }
+
+    return prevValue
+  }, 0)
+
+  function handleAddTask() {
+    if (!inputValue) {
+      return
+    }
+
+    const newTask: ITask = {
+      id: new Date().getTime(),
+      text: inputValue,
+      isChecked: false,
+    }
+
+    setTasks((state) => [...state, newTask])
+    setInputValue('')
+  }
+
+  function handleRemoveTask(id: number) {
+    const filteredTasks = tasks.filter((task) => task.id !== id)
+
+    if (!confirm('Deseja mesmo apagar essa tarefa?')) {
+      return
+    }
+
+    setTasks(filteredTasks)
+  }
+
+  function handleToggleTask({ id, value }: { id: number; value: boolean }) {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isChecked: value }
+      }
+
+      return { ...task }
+    })
+
+    setTasks(updatedTasks)
+  }
+
+  return (
+    <main>
+      <Header />
+
+      <section className={styles.content}>
+        <div className={styles.taskInfoContainer}>
+          <Input
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+          />
+          <Button onClick={handleAddTask}>
+            Criar
+            <PlusCircle size={16} color="#f2f2f2" weight="bold" />
+          </Button>
+        </div>
+
+        <div className={styles.tasksList}>
+          <ListHeader
+            tasksCounter={tasks.length}
+            checkedTasksCounter={checkedTasksCounter}
+          />
+
+          {tasks.length > 0 ? (
+            <div>
+              {tasks.map((task) => (
+                <Item
+                  key={task.id}
+                  data={task}
+                  removeTask={handleRemoveTask}
+                  toggleTaskStatus={handleToggleTask}
+                />
+              ))}
+            </div>
+          ) : (
+            <Empty />
+          )}
+        </div>
+      </section>
+    </main>
+  )
+}
