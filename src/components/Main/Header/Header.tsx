07576ff524648e863e9b893993d41d67df035b44
Yure <yurecod@gmail.com>
2024-06-24 17:33:41 -0400

    add logo 10 anos ipdec

--- src/components/Main/Header/Header.tsx
+++ src/components/Main/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export function Header() {
   return (
     <header className={styles.container}>
-      <img src="/logo.svg" alt="logo da aplicação" />
+      <img src="/logo10anos.png" alt="logo da aplicação" height={60} />
     </header>
   )
 }
